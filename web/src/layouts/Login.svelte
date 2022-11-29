b0d4f74d13f766cd8309cccdd98362879d1494e8
Jeremy Fleming <jeremyfleming281@gmail.com>
2022-11-29 10:38:39 -0500

    fix

---
+++ web/src/layouts/Login.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { Router, Route } from "svelte-routing";
+
+  // components for this layout
+  import AuthNavbar from "components/Navbars/AuthNavbar.svelte";
+  import FooterSmall from "components/Footers/FooterSmall.svelte";
+
+  // pages for this layout
+  import AdminLogin from "views/admin/Login.svelte";
+  import UserLogin from "views/Login.svelte";
+
+
+  const registerBg2 = "../assets/img/AnimatedShape.svg";
+  export let location;
+  export let auth = "";
+</script>
+
+<div>
+  <AuthNavbar />
+  <main>
+    <section class="relative w-full h-full py-40 min-h-screen">
+      <div
+        class="absolute top-0 w-full h-full bg-blueGray-800 bg-no-repeat bg-full"
+        style="background-image: url({registerBg2});"
+      ></div>
+      {#if location == "admin"}
+        <AdminLogin/>
+      {:else}
+        <UserLogin/>
+      {/if}
+    </section>
+  </main>
+</div>
---
+++ web/src/views/Login.svelte
@@ -0,0 +1,76 @@
+<script>
+  import { link } from "svelte-routing";
+
+  // core components
+  const github = "../assets/img/github.svg";
+  const google = "../assets/img/google.svg";
+  export let location;
+</script>
+
+<div class="container mx-auto px-4 h-full">
+  <div class="flex content-center items-center justify-center h-full">
+    <div class="w-full lg:w-4/12 px-4">
+      <div
+        class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0"
+      >
+        <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
+          <div class="text-blueGray-400 text-center mb-3 font-bold mt-6">
+            <h3>Sign in with your credentials</h3>
+          </div>
+          <form method="post" action="//api.digisignonline.com/eafab/signin">
+            <div class="relative w-full mb-3">
+              <label
+                class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                for="grid-email"
+              >
+                UserID
+              </label>
+              <input
+                id="grid-email"
+                type="text"
+                class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                placeholder="UserID"
+              />
+            </div>
+
+            <div class="relative w-full mb-3">
+              <label
+                class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                for="grid-password"
+              >
+                Access Code
+              </label>
+              <input
+                id="grid-password"
+                type="password"
+                class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                placeholder="Access Code"
+              />
+            </div>
+            <div>
+              <label class="inline-flex items-center cursor-pointer">
+                <input
+                  id="customCheckLogin"
+                  type="checkbox"
+                  class="form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150"
+                />
+                <span class="ml-2 text-sm font-semibold text-blueGray-600">
+                  Remember me
+                </span>
+              </label>
+            </div>
+
+            <div class="text-center mt-6">
+              <button
+                class="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+                type="button"
+              >
+                Sign In
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
